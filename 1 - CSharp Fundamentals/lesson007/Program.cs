801177b146bbb044564d9287e25045384b3b8d4d
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-29 11:11:59 +0700

    1.CSharp Fundamentals-lesson007-STRING REVERSER (StringManipulator)

---
+++ 1 - CSharp Fundamentals/lesson007/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== STRING REVERSER ===\n");
+        var manipulator = new StringManipulator();
+
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string reversed = manipulator.ReverseString(input);
+
+        Console.WriteLine("Reversed string: " + reversed);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
+
+public class StringManipulator
+{
+    public string ReverseString(string text)
+    {
+        char[] charArray = text.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
+/*
+ !Explanation of the Solution
+
+    * 1.Convert to char Array:
+
+    ToCharArray() converts the string to a character array, which allows easy manipulation.
+
+    * 2.Reverse the Array:
+
+    Array.Reverse(charArray) reverses the characters in the array in place.
+
+    * 3.Return the Reversed String:
+
+    new string(charArray) creates a new string from the reversed character array.
+ */
