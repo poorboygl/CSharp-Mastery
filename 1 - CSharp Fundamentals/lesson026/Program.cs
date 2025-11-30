0ee269181822e847f41cd724f82cb2327e95171c
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-30 22:19:41 +0700

    1.CSharp Fundamentals-lesson026-RemoveDuplicates Result

---
+++ 1 - CSharp Fundamentals/lesson026/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        var processor = new ListProcessor();
+
+        var numbers = new List<int> { 1, 2, 3, 2, 4, 1, 5, 3 };
+
+        Console.WriteLine("=== RemoveDuplicates Result ===");
+        Console.WriteLine("Input List: " + string.Join(", ", numbers));
+
+        var result = processor.RemoveDuplicates(numbers);
+
+        Console.WriteLine("List Without Duplicates: " + string.Join(", ", result));
+
+        Console.ReadKey();
+    }
+}
+public class ListProcessor
+{
+    public List<int> RemoveDuplicates(List<int> numbers)
+    {
+        var uniqueNumbers = new HashSet<int>();
+
+        foreach (var number in numbers)
+        {
+            uniqueNumbers.Add(number);
+        }
+
+        return new List<int>(uniqueNumbers);
+    }
+}
