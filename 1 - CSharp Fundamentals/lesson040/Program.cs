d58a6c9090521d843092984c18ded417c448b018
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-30 23:41:25 +0700

    1.CSharp Fundamentals-lesson040-DiceSimulator Result

---
+++ 1 - CSharp Fundamentals/lesson040/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        var simulator = new DiceSimulator();
+
+        Console.WriteLine("=== DiceSimulator Result ===");
+
+        string result = simulator.RollDiceAndCheckWin();
+
+        Console.WriteLine("Dice Roll Result: " + result);
+
+        Console.ReadKey();
+    }
+}
+
+public class DiceSimulator
+{
+    public string RollDiceAndCheckWin()
+    {
+        Random random = new Random();
+        int diceRoll = random.Next(1, 7); // Generates a number between 1 and 6
+
+        if (diceRoll == 6)
+        {
+            return "Win";
+        }
+        else
+        {
+            return "Lose";
+        }
+    }
+}
+
+/*
+* 1.Generate Random Dice Roll:
+
+    random.Next(1, 7) generates a random integer from 1 to 6.
+
+* 2.Conditional Check for Win:
+
+    If diceRoll is 6, return "Win".
+
+    Otherwise, return "Lose".
+ 
+ */
